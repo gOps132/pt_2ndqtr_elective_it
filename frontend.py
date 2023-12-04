7238826665737a47f0f78ad72b2d4a45a0a6883a
gOps132 <epilred@icloud.com>
2023-12-04 20:54:23 +0800

    refractored code

---
+++ frontend.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+class InfoQueryCtx:        
+    def __init__(self):
+        self.frame = ttk.Frame()
+        self.frame.grid(column=1)
+
+        self.student_id = tk.Label(master=self.frame, text="Student ID")
+        self.last_name = tk.Label(master=self.frame, text="Last Name")
+        self.first_name = tk.Label(master=self.frame, text="First Name")
+        self.gender = tk.Label(master=self.frame, text="Gender")
+        self.section = tk.Label(master=self.frame, text="Section")
+        self.year_level = tk.Label(master=self.frame, text="Year Level")
+        self.strand = tk.Label(master=self.frame, text="Strand")
+        
+        self.student_id_entry = tk.Entry(master=self.frame)
+        self.last_name_entry = tk.Entry(master=self.frame)
+        self.first_name_entry = tk.Entry(master=self.frame)
+        self.gender_options = tk.Frame(master=self.frame)
+        self.gender_entry_m = ttk.Radiobutton(
+            master=self.gender_options, 
+            text="Male",
+            value="Male",
+            variable=self.gender_options
+        )
+        self.gender_entry_f = ttk.Radiobutton(
+            master=self.gender_options, 
+            text="Female",
+            value="Female",
+            variable=self.gender_options
+        )
+        self.section_entry = ttk.Combobox(
+            master=self.frame,
+            values=["Joswiak", "Kazmierski", "Wojciechowski", "Klin ik"]
+        )
+        self.year_level_entry = ttk.Combobox(
+            master=self.frame,
+            values=["09", "10", "11", "12"]
+        )
+        self.strand_entry = ttk.Combobox(
+            master=self.frame,
+            values=["STEM", "GAS", "ABM"]
+        )
+
+    def grid(self) -> None:
+        self.student_id.grid(row=0, column=0)
+        self.last_name.grid(row=1, column=0)
+        self.first_name.grid(row=2, column=0)
+        self.gender.grid(row=3, column=0)
+        self.section.grid(row=4, column=0)
+        self.year_level.grid(row=5, column=0)
+        self.strand.grid(row=6, column=0)
+
+        self.student_id_entry.grid(row=0, column=1)
+        self.last_name_entry.grid(row=1, column=1)
+        self.first_name_entry.grid(row=2, column=1)
+        self.gender_options.grid(row=3, column=1)
+        self.gender_entry_m.grid(row=0, column=0)
+        self.gender_entry_f.grid(row=0, column=1)
+        self.section_entry.grid(row=4, column=1)
+        self.year_level_entry.grid(row=5, column=1)
+        self.strand_entry.grid(row=6, column=1)
+
+
+class ExecuteQueryCtxWidget:
+    def __init__(self, cnx):
+        self.frame = ttk.Frame()
+        self.frame.grid(column=1)
+        self.cnx = cnx
+
+        self.insert_btn = tk.Button(master=self.frame, text="Insert")    
+        self.update_btn = tk.Button(master=self.frame, text="Update")    
+        self.delete_btn = tk.Button(master=self.frame, text="Delete")    
+        self.queue_btn = tk.Button(
+            master=self.frame,
+            text="Queue",
+            command=self.queue
+        )
+
+    def grid(self):
+        self.insert_btn.grid(row=0, column=0)
+        self.update_btn.grid(row=0, column=1)
+        self.delete_btn.grid(row=0, column=2)
+        self.queue_btn.grid(row=0, column=3)
+    
+    def queue(self):
+        result = self.cnx.queue_db("SELECT * FROM students")
+        print(result) # queued none
---
+++ readme.md
@@ -0,0 +1,33 @@
+# ELECTIVE COURSE GROUP _
+
+## MEMBERS
+* Jess Evangelista
+* Gian Cedrick G. Epilan
+* Andan
+* Arquiza
+* ...
+
+## How to run:
+1. Initialize and run mysql server using [redacted].sql 
+2. Make a Virtual Environment
+```
+    python -m venv .venv
+```
+3. Activate Virtual Environment
+On Powershell
+```
+    .\.venv\Scripts\activate.ps1
+```
+On CMD
+```
+    .\.venv\Scripts\activate.bat
+```
+On Unix (untested)
+```
+    .\.venv\Scripts\activate
+```
+4. Run Python File
+```
+    python main.py
+```
+
